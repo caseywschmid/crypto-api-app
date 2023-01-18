6b2e5f98ce7bbb0c2bbbdf56549a96ef9e3e4fbb
Casey Schmid <caseywschmid@gmail.com>
2023-01-18 07:38:01 +0100

    Added GitHub Pages Support

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./styles.css";
-import {useState, useEffect} from 'react';
+import { useState, useEffect } from "react";
 import Crypto from "./crypto.json";
 
 // API
@@ -13,46 +13,41 @@ import Crypto from "./crypto.json";
 // DOGEUSDT (DogeCoin)
 
 export default function App() {
-
-  
   const COIN_NAMES = {
-    BTCUSDT: 'Bitcoin',
-    ETHUSDT: 'Etherium',
-    SOLUSDT: 'Solana',
-    ADAUSDT: 'Cardano',
-    DOGEUSDT: 'Dogecoin'
+    BTCUSDT: "Bitcoin",
+    ETHUSDT: "Etherium",
+    SOLUSDT: "Solana",
+    ADAUSDT: "Cardano",
+    DOGEUSDT: "Dogecoin",
   };
-  // One quick refactor is by deriving this array from COIN_NAMES
+  // Names are not provided in the JSON so I pulled this array from the
+  // COIN_NAMES array in an attempt to write DRY code.
   const DESIRED_COINS = Object.keys(COIN_NAMES);
-  // const DESIRED_COINS = ['BTCUSDT', 'ETHUSDT', 'SOLUSDT', 'ADAUSDT', 'DOGEUSDT'];
 
-  // 1. STATE AND USEEFFECT HERE
   const [cryptoData, setCryptoData] = useState([]);
 
   async function getCryptoData() {
     try {
-        const response = await fetch('https://api2.binance.com/api/v3/ticker/24hr');
-        const json = await response.json();
-        const filteredCryptoData = json.filter((item) => {
-          if (DESIRED_COINS.includes(item.symbol)) {
-            return true;
-          }
-        });
-        setCryptoData(filteredCryptoData);
-    } catch(error) {
-        console.log(error);
+      const response = await fetch(
+        "https://api2.binance.com/api/v3/ticker/24hr"
+      );
+      const json = await response.json();
+      const filteredCryptoData = json.filter((item) => {
+        if (DESIRED_COINS.includes(item.symbol)) {
+          return true;
+        }
+      });
+      setCryptoData(filteredCryptoData);
+    } catch (error) {
+      console.log(error);
     }
   }
 
+  // Runs when the second argument changes. 
+  // No argument means that it runs only on the first render. 
   useEffect(() => {
     getCryptoData();
-    // fetch('https://api2.binance.com/api/v3/ticker/24hr')
-    // filter the data that you want
-    // setCryptoData(filteredCryptoData);
   }, []); // component did mount
-
-  // Here's how you can cleanly print json to the console:
-  // console.log("I want to add " + JSON.stringify(cryptoToAdd, null, '\t'));
 
   return (
     <div className="App">
@@ -64,7 +59,7 @@ export default function App() {
         <input type="text" placeholder="Search" />
       </nav>
       <div className="main-content">
-        <h2>Today's cryptocurrency prices</h2>
+        <h2>Today's Cryptocurrency Prices</h2>
         <table>
           <thead>
             <tr>
@@ -75,26 +70,25 @@ export default function App() {
             </tr>
           </thead>
           <tbody>
-          {
-            cryptoData.map((coin, i) => {
+            {cryptoData.map((coin, i) => {
               return (
                 <tr key={coin.symbol}>
                   <td>{i + 1}</td>
                   <td>{COIN_NAMES[coin.symbol]}</td>
                   <td>${Number(coin.lastPrice).toFixed(2)}</td>
-                  <td 
-                    style={coin.priceChangePercent > 0 
-                      ? { color: "green" } 
-                      : { color: "red" }}>
-                    {coin.priceChangePercent > 0 
-                      ? '▲' 
-                      : '▼'}
+                  <td
+                    style={
+                      coin.priceChangePercent > 0
+                        ? { color: "green" }
+                        : { color: "red" }
+                    }
+                  >
+                    {coin.priceChangePercent > 0 ? "▲" : "▼"}
                     {Number(coin.priceChangePercent).toFixed(2)}%
                   </td>
                 </tr>
-              )
-            })
-          }
+              );
+            })}
           </tbody>
         </table>
         <div className="bottom-logo-ctr">
